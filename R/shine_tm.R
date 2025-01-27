9dce5c73a671b4a57e217f73e5ae4e00a76c48f3
paskn <pashakhin@gmail.com>
2025-01-27 15:39:43 +0100

    Delete shine_tm
    I have no use for it right now and it adds noise to R CMD check.

--- R/shine_tm.R
+++
@@ -1,9 +0,0 @@
-# Run the shiny app
-shine_tm <- function() {
-  appDir <- system.file("topicl_app", package = "topicl")
-  if (appDir == "") {
-    stop("Could not find the shiny app. Try re-installing `topicl`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal")
-}
